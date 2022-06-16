13b6bd8360b96983a7094f0f19440079f4341204
Joe Roberts <joe.t.roberts@jpl.nasa.gov>
2022-06-16 15:58:44 -0700

    Ability to use config file, convert uv components, and code cleanup

--- netcdf-geojson-vectors.py
+++ netcdf-geojson-vectors.py
@@ -20,34 +20,89 @@ Convert CF-compliant NetCDF files with vector attributes to GeoJSON
 """
 import argparse
 import os
+import json
 import geopandas
+import numpy as np
 import xarray as xr
 from pathlib import Path
 
 
-def netcdf2geojson(config_file, input_file, output_dir):
+def netcdf2geojson(config_file, input_file, output_dir, max_records=None):
+    """Converts NetCDF file to GeoJSON based on config"""
+    conf = read_config(config_file)
 
     if os.path.exists(output_dir) is False:
         os.makedirs(output_dir)
-
-    output_file = output_dir + '/' + Path(input_file).stem + '.json'
+    output_file = Path(output_dir).joinpath(Path(input_file).stem + '.json')
     print(f'Reading {input_file}')
+
     local_dataset = xr.open_dataset(input_file)
     df = local_dataset.to_dataframe()
     df = df.dropna(how='any', axis=0).reset_index()
-    df['lon'] = (df['lon'] + 180) % 360 - 180
-    print(df)
 
-    data = {'u': df.u,
-            'v': df.v}
+    if max_records is not None:
+        df = df[0: int(max_records)]
+
+    if conf['is360'] is True:
+        df[conf['lonVar']] = (df[conf['lonVar']] + 180) % 360 - 180
+
+    print(df)
+    data = {}
+
+    if conf.get('speedVar') and conf.get('dirVar'):
+        print('Using speedVar and dirVar')
+        data['speed'] = df[conf['speedVar']]
+        data['dir'] = df[conf['dirVar']]
+
+    if conf.get('uVar') and conf.get('vVar'):
+        if conf['convertUV'] is True:
+            print('Calculating speed and direction from uVar and vVar')
+            data['speed'] = df.apply(lambda x: uv2speed(x[conf['uVar']], x[conf['vVar']]), axis=1)
+            data['dir'] = df.apply(lambda x: uv2dir(x[conf['uVar']], x[conf['vVar']]), axis=1)
+        else:
+            print('Using uVar and vVar')
+            data['u'] = df[conf['uVar']]
+            data['v'] = df[conf['vVar']]
 
     print('Converting to GeoJSON')
-    gdf = geopandas.GeoDataFrame(data, geometry=geopandas.points_from_xy(df.lon, df.lat))
+    gdf = geopandas.GeoDataFrame(data, geometry=geopandas.points_from_xy(df[conf['lonVar']], df[conf['latVar']]))
     print(gdf)
 
     print(f'Writing {output_file}')
     gdf.to_file(f'{output_file}', driver="GeoJSON")
     print(f'Created {output_file}')
+
+
+def read_config(config_file):
+    """Parses a JSON dataset config file"""
+    config = None
+    with open(config_file) as config_f:
+        config = json.load(config_f)
+    return config
+
+
+def uv2dir(u, v):
+    """
+    Calculates direction from u and v components
+    Parameters
+    ----------
+    u = west/east direction
+    v = south/north direction
+    """
+    dir = (270 - np.rad2deg(np.arctan2(v, u))) % 360
+    return dir
+
+
+def uv2speed(u, v):
+    """
+    Calculates speed from u and v wind components
+    Parameters
+    ----------
+    u = west/east direction
+    v = south/north direction
+    """
+    speed = np.sqrt(np.square(u) + np.square(v))
+    return speed
 
 
 parser = argparse.ArgumentParser(description='Convert CF-compliant NetCDF files with vector attributes to GeoJSON.')
@@ -58,10 +113,22 @@ parser.add_argument(
     action='store',
     help='Configuration file')
 parser.add_argument(
+    '-d',
+    '--input_dir',
+    dest='input_dir',
+    help='Directory containing input files',
+    action='store')
+parser.add_argument(
     '-i',
     '--input_file',
     dest='input_file',
     help='Input file',
+    action='store')
+parser.add_argument(
+    '-m',
+    '--max_records',
+    dest='max_records',
+    help='Maximum number of records to process',
     action='store')
 parser.add_argument(
     '-o',
@@ -71,9 +138,11 @@ parser.add_argument(
     help='Output directory',
     action='store')
 
-
 args = parser.parse_args()
-
-netcdf2geojson(args.config_file,
-               args.input_file,
-               args.output_dir)
+if args.input_dir:
+    input_files = None
+else:
+    netcdf2geojson(args.config_file,
+                   args.input_file,
+                   args.output_dir,
+                   args.max_records)
